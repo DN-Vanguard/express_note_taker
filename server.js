99030b2ba71abbfc90fe5c2f32812fe98f640bdd
DN-Vanguard <Domiltannguyen@outlook.com>
2021-07-26 22:46:41 -0700

    server.js was added and completed

---
+++ server.js
@@ -0,0 +1,25 @@
+// Required modules
+const express = require( 'express' );
+const path = require( 'path' );
+const routes = require( './routes/index.js' );
+
+// Open port on Heroku or default to 3001;
+const PORT = process.env.PORT || 3001;
+
+// Load Express.js to app
+const app = express();
+
+// Middleware
+app.use( express.json() );
+app.use( express.urlencoded( { extended: true } ));
+
+// Open access to file directory 'public'
+app.use( express.static( 'public' ));
+
+// Route requests to index.js
+app.use( '/', routes );
+
+// Listening port
+app.listen(PORT, () =>
+  console.log( `Server open at http://localhost:${PORT}` )
+);

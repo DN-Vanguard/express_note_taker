4d65a2480524a01a939551b83dbb090d28829683
DN-Vanguard <Domiltannguyen@outlook.com>
2021-07-26 23:07:05 -0700

    added more route data and requests

--- server.js
+++ server.js
@@ -23,11 +23,68 @@ const overwriteDBFile = (noteCollection) => {
         console.log('Saved db.json');
     });
 };
-
+// route
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
-
+// return the current notes by reading `db.json`
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.err(err);
+        } else {
+            res.json(JSON.parse(data))
+        }
+    });
+});
+app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+    if (req.body) {
+        // creates a new note object with a unique ID and the user input
+        const newNote = {
+            title,
+            text,
+            id: uuidv4()
+        };
+        // read file then append new note
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.err(err);
+            } else {
+                const noteCollection = JSON.parse(data); // save db.json data into an array
+                noteCollection.push(newNote); // add new note object to the array 
+                overwriteDBFile(noteCollection); // save the new data to db.json
+                res.json(newNote); // return new note to client
+            }
+        });
+    } else {
+        res.error('Error adding Note');
+    };
+});
+// delete
+app.delete('/api/notes/:id', (req, res) => {
+    if (req.params.id) {
+        const noteId = req.params.id;
+        // read file then delete note with matching id
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.err(err);
+            } else {
+                const noteCollection = JSON.parse(data); // save db.json data into an array
+                for (let i = 0; i < noteCollection.length; i++) {
+                    if (noteCollection[i].id === noteId) {
+                        noteCollection.splice([i], 1);
+                        break; // stop iterating once a unique id is found
+                    };
+                };
+                overwriteDBFile(noteCollection); // save the new data to db.json
+            };
+        });
+        res.send(`Deleted`);
+    } else {
+        res.error('Error deleting note')
+    };
+});
 
 // Listening port
 app.listen(PORT, () =>
